1071c1ba2961e01f32b94c2e9a7aa5d82f040269
Derechei <work25th91@gmail.com>
2015-08-23 20:13:07 +0300

    created project structure and gulp config-file

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+var gulp = require('gulp'),
+    scss = require('gulp-ruby-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    prefixer = require('gulp-autoprefixer'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    jsuglify = require('gulp-uglify'),
+    cssuglify = require('gulp-uglifycss'),
+    htmluglify = require('gulp-minify-html');
+
+gulp.task('default', ['imagemin', 'scss', 'prefixer', 'watch']);
+
+// Gulp watcher.
+gulp.task('watch', function () {
+    livereload.listen();
+    gulp.watch('scss_styles/*.scss', ['scss']);
+    gulp.watch('css/all.css', ['prefixer']);
+});
+
+// SCSS Task compiler.
+gulp.task('scss', function () {
+    return scss('scss_styles/', {
+        sourcemap: true,
+        cacheLocation: 'scss_styles/sass_cache',
+        force: true
+    })
+        .pipe(sourcemaps.write('/', {
+            includeContent: false,
+            sourceRoot: 'scss_styles/'
+        }))
+        .pipe(gulp.dest('css/styles'))
+        .pipe(livereload());
+});
+
+// Autoprefixer task.
+gulp.task('prefixer', function () {
+    return gulp.src('css/*/*.css')
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(prefixer({
+            cascade: true,
+            remove: true,
+            browsers: [
+                'last 3 versions'
+            ]
+        }))
+        .pipe(concat('all.css'))
+        .pipe(sourcemaps.write('/', {sourceRoot: true}))
+        .pipe(gulp.dest('css'));
+});
+
+// ImageMin task.
+gulp.task('imagemin', function () {
+    return gulp.src('content/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            multypass: true,
+            svgoPlugins: [
+                {removeViewBox: false},
+                {removeUselessStrokeAndFill: false},
+                {removeEmptyAttrs: false}
+            ],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('content/img/'));
+});
+
+// Minify all.
+gulp.task('minify', ['jsuglify', 'cssuglify', 'htmluglify']);
+
+// JSUglify task.
+gulp.task('jsuglify', function () {
+    return gulp.src('js/*.js')
+        .pipe(jsuglify({
+            output: { quote_keys: true },
+            compress: {hoist_vars: true}
+        }))
+        .pipe(gulp.dest('minify'));
+});
+
+// CSSUglify task.
+gulp.task('cssuglify', function () {
+    gulp.src('css/all.css')
+        .pipe(cssuglify({
+            maxLineLen: 32000,
+            expandVars: false,
+            uglyComments: true
+        }))
+        .pipe(gulp.dest('minify'));
+});
+
+// HTMLUglify task.
+ gulp.task('htmluglify', function () {
+     var opts = {
+         conditionals: true,
+         spare:true,
+         empty: true,
+         cdata: true
+     };
+     return gulp.src('html/*.html')
+         .pipe(htmluglify(opts))
+         .pipe(gulp.dest('minify'));
+ });
