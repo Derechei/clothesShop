2520c4f567cf924aa4b77fd3059d5c0f83eef95b
Derechei <work25th91@gmail.com>
2015-08-30 03:02:35 +0300

    fix scroll event aboveMenu

--- js/catalogue.js
+++ js/catalogue.js
@@ -69,7 +69,7 @@ var EPAM =
         // Resize header to show scroll onload and each time after resizing window.
         window.addEventListener('resize', scrollShow, false);
         window.addEventListener('load', initDoc, false);
-        function initDoc(){
+        function initDoc() {
             if (window.innerWidth > 550) {
                 header.aboveNavigation.self.className = 'ABOVE_HEADER collapse show';
             }
@@ -112,139 +112,78 @@ var EPAM =
                 target: null,
                 targetImg: null,
                 img: null,
-                a: null
+                a: null,
+                event: e
             };
             switch (e.target) {
-                // For dt elements.
-                case header.belowNavigation.menu.men:
+                case document.querySelector('.MEN a') && document.querySelector('.MEN span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.men,
                         targetImg: document.querySelectorAll('.menu_choose')[1],
                         img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.MEN>a')
+                        a: document.querySelector('.MEN>a'),
+                        event: e
                     };
                     onMouseMoveHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.woman:
+                case document.querySelector('.WOMAN a') && document.querySelector('.WOMAN span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.woman,
                         targetImg: document.querySelectorAll('.menu_choose')[0],
                         img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.WOMAN>a')
+                        a: document.querySelector('.WOMAN>a'),
+                        event: e
                     };
                     onMouseMoveHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.kids:
+                case document.querySelector('.KIDS a') && document.querySelector('.KIDS span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.kids,
                         targetImg: document.querySelectorAll('.menu_choose')[1],
                         img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.KIDS>a')
+                        a: document.querySelector('.KIDS>a'),
+                        event: e
                     };
                     onMouseMoveHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.sports:
+                case document.querySelector('.SPORTS a') && document.querySelector('.SPORTS span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.sports,
                         targetImg: document.querySelectorAll('.menu_choose')[0],
                         img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.SPORTS>a')
+                        a: document.querySelector('.SPORTS>a'),
+                        event: e
                     };
                     onMouseMoveHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.blog:
+                case document.querySelector('.BLOG a') && document.querySelector('.BLOG span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.blog,
                         targetImg: document.querySelectorAll('.menu_choose')[1],
                         img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.BLOG>a')
+                        a: document.querySelector('.BLOG>a'),
+                        event: e
                     };
                     onMouseMoveHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.catalog:
+                case document.querySelector('.CATALOG>a') && document.querySelector('.CATALOG span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.catalog,
                         targetImg: document.querySelectorAll('.menu_choose')[0],
                         img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.CATALOG>a')
-                    };
-                    onMouseMoveHelper(helperParam);
-                    break;
-                }
-                // For a elements.
-                case document.querySelector('.MEN>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.men,
-                        targetImg: document.querySelectorAll('.menu_choose')[1],
-                        img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.MEN>a')
-                    };
-                    onMouseMoveHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.WOMAN>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.woman,
-                        targetImg: document.querySelectorAll('.menu_choose')[0],
-                        img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.WOMAN>a')
-                    };
-                    onMouseMoveHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.KIDS>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.kids,
-                        targetImg: document.querySelectorAll('.menu_choose')[1],
-                        img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.KIDS>a')
-                    };
-                    onMouseMoveHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.SPORTS>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.sports,
-                        targetImg: document.querySelectorAll('.menu_choose')[0],
-                        img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.SPORTS>a')
-                    };
-                    onMouseMoveHelper(helperParam);
-                    break;
-                }
-                case  document.querySelector('.BLOG>a') && document.querySelector('.BLOG strong'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.blog,
-                        targetImg: document.querySelectorAll('.menu_choose')[1],
-                        img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.BLOG>a')
-                    };
-                    onMouseMoveHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.CATALOG>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.catalog,
-                        targetImg: document.querySelectorAll('.menu_choose')[0],
-                        img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.CATALOG>a')
+                        a: document.querySelector('.CATALOG>a'),
+                        event: e
                     };
                     onMouseMoveHelper(helperParam);
                     break;
@@ -258,13 +197,22 @@ var EPAM =
             o['targetImg'].style.width = o['target'].offsetWidth + 'px';
             o['targetImg'].style.margin = '0';
             if (o['target'] === header.belowNavigation.menu.blog) {
-                o['targetImg'].style.marginLeft = parseInt(window.getComputedStyle(header.belowNavigation.menu.blog).marginLeft) - 2.5 + 'px';
+                o['targetImg'].style.marginLeft = parseInt(window.getComputedStyle(header.belowNavigation.menu.blog).marginLeft) + 'px';
             }
             o['targetImg'].style.display = 'block';
             o['targetImg'].style.height = '8px';
             o['img'].style.display = 'none';
             o['a'].className = 'hover';
+            if ( o.target !== header.belowNavigation.menu.blog ) {
+                document.querySelector('.dropmenu').className = 'dropmenu show';
+            }
+            o.event.stopPropagation();
+            o.event.preventDefault();
         }
+
+        /*function hideDropMenu(){
+            document.querySelector('.dropmenu').className = 'dropmenu';
+        }*/
 
         // Mouseout.
         header.belowNavigation.menu.self.addEventListener('mouseout', menuOnMouseOut, false);
@@ -273,139 +221,79 @@ var EPAM =
                 target: null,
                 targetImg: null,
                 img: null,
-                a: null
+                a: null,
+                event: e
             };
             switch (e.target) {
-                // For dt elements.
-                case header.belowNavigation.menu.men:
+                case document.querySelector('.MEN a') && document.querySelector('.MEN span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.men,
                         targetImg: document.querySelectorAll('.menu_choose')[1],
                         img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.MEN>a')
+                        a: document.querySelector('.MEN>a'),
+                        event: e
                     };
                     onMouseOutHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.woman:
+                case document.querySelector('.WOMAN a') && document.querySelector('.WOMAN span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.woman,
                         targetImg: document.querySelectorAll('.menu_choose')[0],
                         img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.WOMAN>a')
+                        a: document.querySelector('.WOMAN>a'),
+                        event: e
                     };
                     onMouseOutHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.kids:
+                case document.querySelector('.KIDS a') && document.querySelector('.KIDS span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.kids,
                         targetImg: document.querySelectorAll('.menu_choose')[1],
                         img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.KIDS>a')
+                        a: document.querySelector('.KIDS>a'),
+                        event: e
                     };
                     onMouseOutHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.sports:
+                case document.querySelector('.SPORTS a') && document.querySelector('.SPORTS span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.sports,
                         targetImg: document.querySelectorAll('.menu_choose')[0],
                         img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.SPORTS>a')
+                        a: document.querySelector('.SPORTS>a'),
+                        event: e
                     };
                     onMouseOutHelper(helperParam);
                     break;
                 }
-                case header.belowNavigation.menu.blog:
+                case document.querySelector('.BLOG a') && document.querySelector('.BLOG span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.blog,
                         targetImg: document.querySelectorAll('.menu_choose')[1],
                         img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.BLOG>a')
+                        a: document.querySelector('.BLOG>a'),
+                        event: e
                     };
                     onMouseOutHelper(helperParam);
+                    console.log(e.target);
                     break;
                 }
-                case header.belowNavigation.menu.catalog:
+                case document.querySelector('.CATALOG>a') && document.querySelector('.CATALOG span'):
                 {
                     helperParam = {
                         target: header.belowNavigation.menu.catalog,
                         targetImg: document.querySelectorAll('.menu_choose')[0],
                         img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.CATALOG>a')
-                    };
-                    onMouseOutHelper(helperParam);
-                    break;
-                }
-                // For a elements.
-                case document.querySelector('.MEN>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.men,
-                        targetImg: document.querySelectorAll('.menu_choose')[1],
-                        img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.MEN>a')
-                    };
-                    onMouseOutHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.WOMAN>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.woman,
-                        targetImg: document.querySelectorAll('.menu_choose')[0],
-                        img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.WOMAN>a')
-                    };
-                    onMouseOutHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.KIDS>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.kids,
-                        targetImg: document.querySelectorAll('.menu_choose')[1],
-                        img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.KIDS>a')
-                    };
-                    onMouseOutHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.SPORTS>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.sports,
-                        targetImg: document.querySelectorAll('.menu_choose')[0],
-                        img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.SPORTS>a')
-                    };
-                    onMouseOutHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.BLOG>a') && document.querySelector('.BLOG strong'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.blog,
-                        targetImg: document.querySelectorAll('.menu_choose')[1],
-                        img: document.querySelectorAll('.menu_choose')[0],
-                        a: document.querySelector('.BLOG>a')
-                    };
-                    onMouseOutHelper(helperParam);
-                    break;
-                }
-                case document.querySelector('.CATALOG>a'):
-                {
-                    helperParam = {
-                        target: header.belowNavigation.menu.catalog,
-                        targetImg: document.querySelectorAll('.menu_choose')[0],
-                        img: document.querySelectorAll('.menu_choose')[1],
-                        a: document.querySelector('.CATALOG>a')
+                        a: document.querySelector('.CATALOG>a'),
+                        event: e
                     };
                     onMouseOutHelper(helperParam);
                     break;
@@ -418,28 +306,46 @@ var EPAM =
 
         function onMouseOutHelper(o) {
             o['a'].className = '';
+
+            document.querySelector('.dropmenu').className = 'dropmenu';
+            o.event.stopPropagation();
+            o.event.preventDefault();
         }
 
         document.body.addEventListener('mousemove', showLoginMenu, false);
         function showLoginMenu(e) {
+            document.querySelector("[type='search']").placeholder = e.screenX + " " + e.screenY;
+
             if (window.scrollY === 0 && window.innerWidth > 550) {
                 header.aboveNavigation.self.className = 'ABOVE_HEADER collapse show';
-            }
-            if (window.scrollY >= 50){
-                if (e.screenY <= 200 && window.innerWidth > 550) {
+                scrollShow();
+            } else {
+                if (e.screenY <= 140 && window.innerWidth > 550) {
                     header.aboveNavigation.self.className = 'ABOVE_HEADER collapse show';
+                    scrollShow();
                 } else {
-                    if (e.screenY >= 300 && window.innerWidth > 550) {
+                    if (window.innerWidth > 550) {
                         header.aboveNavigation.self.className = 'ABOVE_HEADER collapse';
                         scrollShow();
                     }
                 }
-            } else {
-                if (e.screenY <= 200 && window.innerWidth > 550) {
-                    header.aboveNavigation.self.className = 'ABOVE_HEADER collapse show';
-                }
             }
         }
+
+        document.body.onscroll = function myFunck(e){
+            console.log(window.scrollY);
+            if (window.scrollY === 0 && window.innerWidth > 550) {
+                header.aboveNavigation.self.className = 'ABOVE_HEADER collapse show';
+                scrollShow();
+            }
+            if (window.scrollY >= 50) {
+
+                if (e.screenY <= 200 && window.innerWidth > 550) {
+                    header.aboveNavigation.self.className = 'ABOVE_HEADER collapse show';
+                    scrollShow();
+                }
+            }
+        };
 
         return header;
     }());
